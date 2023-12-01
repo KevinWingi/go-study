858d80b3b8ba2398ad2fef5a63b1ed6db81e6dd8
KevinWingi <KevinWing8@gmail.com>
2023-12-02 01:32:04 +0200

    Add struct use case

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module eureka
 
 go 1.21.4
+
+require github.com/google/uuid v1.4.0
--- main.go
+++ main.go
@@ -4,31 +4,50 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/google/uuid"
 )
+
+type Person struct {
+	Name string
+	Id   string
+	Age  int
+}
 
 func sum(a int, b int) (int, bool) {
 	return a + b, a > b
 }
 
 func main() {
-	print()
-	server()
+	// print()
+	// server()
+	structcase()
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("HELLO WORLD"))
 }
 
-func server() {
+func servercase() {
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
 }
 
-func print() {
+func printcase() {
 	myVar := "Hello  world"
 	println(myVar)
 
 	sum, status := sum(1, 0)
 
 	fmt.Printf("Sum = %d, First greather than second = %s", sum, strconv.FormatBool((status)))
+}
+
+func structcase() {
+	person := Person{
+		Name: "Kevin Wingi",
+		Id:   uuid.New().String(),
+		Age:  28,
+	}
+
+	fmt.Printf("Name: %s, Id: %s, Age: %d", person.Name, person.Id, person.Age)
 }
